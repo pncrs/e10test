7062b44c08cc0c5ed6188159ddb3b16643c65361
pncrs <pancras@pouw.com>
2018-06-04 16:08:48 +0200

    Add files via upload

---
+++ 201805007.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en" xmlns:fb="http://ogp.me/ns/fb#" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta content="IE=edge, chrome=1" http-equiv="X-UA-Compatible">
+	<meta content="text/html; charset=UTF-8" http-equiv="content-type">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Senior Technical Marketing Automation Consultant France / Belgium | Careers | Engagement Factory</title>
+	<meta name="description" content="Are you a junior technical consultant and looking for your next challenge?">
+	<meta name="keywords" content="career, job, marketing, marketing automation, consultant, marketing, project manager, france">
+	<link rel="shortcut icon" type="image/x-icon" href="https://engagementfactory.blob.core.windows.net/admin-assets/frontend/layout/img/favicon.png">
+	<link rel='stylesheet' href='https://engagementfactory.com/assets/css/style.css?v=2' type='text/css' media='all' />
+</head>
+<body>
+	<?php include('../includes/header.php'); ?>
+	<div class="single-header blue pattern">
+		<div class="row abs">
+			<div class="row-height">
+				<div class="col-md-2 hide-mobile"></div>
+				<div class="col-md-8 col-height col-middle text-center">
+					<h1><li>Senior <br> Technical Marketing Automation <br> Consultant - France/Belgium</li></h1>
+				</div>
+				<div class="col-md-2 hide-mobile"></div>
+			</div>
+		</div>
+	</div>
+	<div class="content-small-wrap padding">
+		<div class="row">
+			<div class="col-md-12">
+				<p class="lb17"><strong>Are you an experience technical consultant and looking for your next challenge? Engagement Factory is seeking a passionate and talented technical Marketing Automation Consultant to join the team.</strong></p>
+				<p>As a technical Consultant, you will be responsible for enabling Modern Marketers by assisting and guiding them through the implementation and configuration of Oracle Marketing Cloud.</p>
+				<p>The ideal candidate would have previous experience with databases (data modeling/data analysis), process modelling and design, system configuration, use case gathering, testing and verification. The candidate must have strong technical problem solving abilities, good understanding of architectural system design and system development methodologies.</p>
+				<p>Knowledge of marketing and sales automation platforms like Eloqua, Salesforce, Marketo, Pardot, Exact Target is required.</p>
+			</div>
+			<div class="spacer-20"></div>
+			<div class="col-md-12">
+				<h3>Responsibilities</h3>
+				<ul class="styled-ul">
+					<li>Configuration and project task management of client implementations in person and remotely.</li>
+					<li>Manage multiple, concurrent tasks/projects with competing timelines</li>
+					<li>Ensure tasks/projects are delivered with integrity and to the highest degree of client satisfaction</li>
+					<li>Meet or exceed KPI targets</li>
+					<li>Aid with technical system design, application setup and implementation</li>
+					<li>Learn and become proficient in Marketing Automation, Service and Sales alignment and “MarTech” technology space.</li>
+					<li>Provide support and technical guidance to all team members as needed</li>
+					<li>Identity and implement internal process improvements</li>
+				</ul>
+			</div>
+			<div class="spacer-20"></div>
+			<div class="col-md-12">
+				<h3>Desired Experience and Skills</h3>
+				<ul class="styled-ul">
+					<li>Minimum of 4-7 years of technical system experience, specifically database setup, system configuration and testing, process automation, web, social media or related area or previous SaaS implementation experience</li>
+					<li>Project/task management experience with multiple, concurrent projects </li>
+					<li>Strong presentation and communication skills</li>
+					<li>Self-starter who can work independently with a passion for “Doing it right and Getting it done”</li>
+					<li>BA/BS in Marketing, Business, Computer Science or Information Technology</li>
+					<li>Good understanding HTML, CSS or JavaScript is a plus</li>
+                    <li>Fluent French speaker</li>
+				</ul>
+			</div>			
+			<div class="spacer-50"></div>
+			<div class="col-md-12 text-center">
+				<a href="mailto:jobs@engagementfactory.com?subject=Senior Technical Marketing Automation Consultant FR/BE – 201805007" target="_self" class="btn btn-orange">Drop us an email!</a>
+			</div>
+			<div class="spacer-50"></div>
+		</div>
+	</div>
+	<?php $ref = 'Careers'; include('../includes/newsletter.php'); ?>
+	<?php include('../includes/footer.php'); ?>
+</body>
+</html>
